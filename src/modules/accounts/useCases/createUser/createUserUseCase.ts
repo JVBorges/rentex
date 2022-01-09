0a5a1871fc2ed646a3594aba9d5390b77ccbce6e
JVBorges <joao-salves@hotmail.com>
2022-01-09 18:36:49 -0300

    feature: user password encryption

--- src/modules/accounts/useCases/createUser/createUserUseCase.ts
+++ src/modules/accounts/useCases/createUser/createUserUseCase.ts
@@ -1,4 +1,5 @@
 import { inject, injectable } from "tsyringe";
+import { hash } from "bcrypt";
 import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 
@@ -7,7 +8,13 @@ class CreateUserUseCase {
   constructor(@inject('UsersRepository') private usersRepository: IUsersRepository) {}
 
   async execute(data: ICreateUserDTO): Promise<void> {
-    await this.usersRepository.create(data);
+    const foundedUser = await this.usersRepository.findByEmail(data.email);
+
+    if (foundedUser)
+      throw new Error('User already exists!');
+
+    const passwordHash = await hash(data.password, 8);
+    await this.usersRepository.create({ ...data, password: passwordHash });
   }
 }
 

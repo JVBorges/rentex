ec6254cf8a2ba7f30b39600e3c3cb34db89c6a3c
JVBorges <joao-salves@hotmail.com>
2022-01-23 21:33:00 -0300

    fix: TypeORM connection adjustments

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -1,11 +1,10 @@
 import { Connection, createConnection, getConnectionOptions } from "typeorm";
 
-export default async (host = 'postgres'): Promise<Connection> => {
+export default async (): Promise<Connection> => {
   const defaultOpts = await getConnectionOptions();
 
   return createConnection(
     Object.assign(defaultOpts, {
-      host: process.env.NODE_ENV === 'test' ? 'localhost' : host,
       database: process.env.NODE_ENV === 'test' ? 'rentex_test' : defaultOpts.database
     })
   );
